470b3c41be86cb9282faa965ad137211949be69f
fvj5054 <fvj5054@gmail.com>
2025-02-21 09:58:05 -0800

    Initial Commit

---
+++ YouAreAwesome1/YouAreAwesome1App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome1App.swift
+//  YouAreAwesome1
+//
+//  Created by Student1 on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
